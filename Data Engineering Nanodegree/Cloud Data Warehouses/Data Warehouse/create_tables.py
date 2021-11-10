9e23b3a0d638b7066902f74099cc62f06bc98aff
Ahmad Mohammed Abd Elhameed <abdelhameed9@yahoo.com>
2021-11-10 08:47:21 +0400

    Adding create_tables file

--- Data Engineering Nanodegree/Cloud Data Warehouses/Data Warehouse/create_tables.py
+++ Data Engineering Nanodegree/Cloud Data Warehouses/Data Warehouse/create_tables.py
@@ -4,18 +4,47 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Description: This function is responsible for deleting pre-existing tables to be able to create them from scratch
+
+    Arguments:
+        cur: the cursor object.
+        conn: connection to the database.
+
+    Returns:
+        None
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Description: This function is responsible for creating staging and dimensional tables from scratch
+
+    Arguments:
+        cur: the cursor object.
+        conn: connection to the database.
+
+    Returns:
+        None
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
-def main():
+def main(): 
+    """
+    Description: This function is responsible for setting up the database tables, create needed tables with the appropriate columns and constricts
+
+    Arguments:
+        None.
+
+    Returns:
+        None
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
